60b17ded4d593707d9d59fddd22ea632658ce175
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-02-11 20:06:33 -0500

    make stasis lazy

--- wazo_stt/calld/plugin.py
+++ wazo_stt/calld/plugin.py
@@ -1,6 +1,9 @@
 # Copyright 2019-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+
+from xivo.pubsub import CallbackCollector
+
 from .notifier import SttNotifier
 from .stasis import SttStasis
 from .services import SttService
@@ -20,7 +23,11 @@ class Plugin:
 
         notifier = SttNotifier(bus_publisher)
         stt_service = SttService(config, ari.client, notifier)
-        stasis = SttStasis(config, ari.client, stt_service)
+        stasis = SttStasis(config, ari, stt_service)
+
+        startup_callback_collector = CallbackCollector()
+        ari.client_initialized_subscribe(startup_callback_collector.new_source())
+        startup_callback_collector.subscribe(stasis.initialize)
 
         api.add_resource(SttCreateResource, '/stt', resource_class_args=[stt_service])
         api.add_resource(SttResource, '/stt/<call_id>', resource_class_args=[stt_service])
--- wazo_stt/calld/stasis.py
+++ wazo_stt/calld/stasis.py
@@ -9,15 +9,13 @@ logger = logging.getLogger(__name__)
 class SttStasis:
 
     def __init__(self, config, ari, stt_service):
-        self._ari = ari
+        self._ari = ari.client
         self._stt_service = stt_service
 
-        if config["stt"]["stasis"]:
-            self.initialize()
-
     def initialize(self):
-        self._ari.on_channel_event('StasisStart', self._stasis_start)
-        logger.debug('Stasis stt initialized')
+        if config["stt"]["stasis"]:
+          self._ari.on_channel_event('StasisStart', self._stasis_start)
+          logger.debug('Stasis stt initialized')
 
     def _stasis_start(self, event_objects, event):
         logger.critical("event_objects: %s", event_objects)
