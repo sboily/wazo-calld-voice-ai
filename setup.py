d54e0f33dbab81df6f22148434ccb55ac4e1d610
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-09-08 19:59:39 -0400

    use plugin.yml for update information in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
+
+import yaml
 
 from setuptools import setup
 from setuptools import find_packages
 
 
+with open('wazo/plugin.yml') as file:
+    metadata = yaml.load(file)
+
 setup(
-    name='wazo-stt',
-    version='0.2.6',
-    description='Wazo STT',
-    author='Wazo Authors',
-    url='http://wazo.community',
+    name=metadata['name'],
+    version=metadata['version'],
+    description=metadata['display_name'],
+    author=metadata['author'],
+    url=metadata['homepage'],
+
     packages=find_packages(),
     include_package_data=True,
     package_data={'wazo_stt': ['*/api.yml']},
